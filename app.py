8405547743b61e009a18dfa048611cbb3f79f09a
Shoichiro Takahashi <lexion13@gmail.com>
2017-08-26 16:43:53 +0000

    fix graph and api

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template,request,redirect,url_for # For flask im
 from pymongo import MongoClient # Database connector
 from bson.objectid import ObjectId # For ObjectId to work
 import os
+import json
+import requests
 
 client = MongoClient('localhost', 27017)    #Configure the connection to the database
 db = client.cryptocurrency
@@ -25,8 +27,31 @@ def lists():
 
 @app.route('/<currency>')
 def currency(currency):
+
+	'''
+	Get price histo
+	
+	'''
+	histoday = []
+	api = "https://min-api.cryptocompare.com/data/histoday?fsym={fsym}&tsym=USD&limit={limit}&aggregate=3&e=CCCAGG"
+	fsym = currency
+	limit = '60'
+	url = api.format(fsym=fsym, limit=limit)
+	pricedata = requests.get(url)
+	pricedata_json = json.loads(pricedata.text)
+	data = pricedata_json["Data"]
+	for e in data:
+		ti = e["time"]*0.0001
+		op = e["open"]
+		hi = e["high"]
+		lo = e["low"]
+		cl = e["close"]
+		vf = e["volumefrom"]
+		vt = e["volumeto"]
+		histoday.append([ti,op,hi,lo,cl])
+
 	currency = collist.find_one({'symbol': currency})
-	return render_template('currency.html', currency=currency)
+	return render_template('currency.html', currency=currency,histoday=histoday)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
